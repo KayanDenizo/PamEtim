1f29ccc22726f72526b46209f7a6e7de831de87e
Android Studio <Android@uirapuru.br>
2025-03-13 15:08:42 -0300

    d

--- app/src/main/java/com/example/etimpamiappdozero/LoginActivity.kt
+++ app/src/main/java/com/example/etimpamiappdozero/LoginActivity.kt
@@ -1,9 +1,15 @@
 package com.example.etimpamiappdozero
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var email:EditText
+    private lateinit var senha:EditText
+    private lateinit var entrar:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
